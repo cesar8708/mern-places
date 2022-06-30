242ecfbd9bbd0dc9524e821488a84acc2a5e4b59
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-06-30 01:03:31 -0500

    User module incompleted

--- my-places/src/user/pages/Users.js
+++ my-places/src/user/pages/Users.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import UserList from "../components/UserList";
 const Users = () => {
-    return <h1>Users Works!</h1>
+    const USERS=[
+        {id:Math.random, name:'Cesar Meza', image:'../../../public/People1.png', places:1},
+        {id:Math.random, name:'Leidy Quiroz', image:'../../../public/People2.png', places:2},
+        {id:Math.random, name:'Nicolás Meza', image:'../../../public/People3.png', places:3},
+    ];
+    return <UserList items={USERS}/>
 }
 
 export default Users;
