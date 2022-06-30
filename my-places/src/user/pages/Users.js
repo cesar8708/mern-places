319184d2bcb397585438438de3bd8530e03328ee
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-06-30 01:32:57 -0500

    list users ok - its pending update de url for images

--- my-places/src/user/pages/Users.js
+++ my-places/src/user/pages/Users.js
@@ -2,10 +2,11 @@ import React from "react";
 import UserList from "../components/UserList";
 const Users = () => {
     const USERS=[
-        {id:Math.random, name:'Cesar Meza', image:'../../../public/People1.png', places:1},
-        {id:Math.random, name:'Leidy Quiroz', image:'../../../public/People2.png', places:2},
-        {id:Math.random, name:'Nicolás Meza', image:'../../../public/People3.png', places:3},
+        {id:Math.random, name:'Cesar Meza', image:'%PUBLIC_URL%/People1.png', places:1},
+        {id:Math.random, name:'Leidy Quiroz', image:'%PUBLIC_URL%/People2.png', places:2},
+        {id:Math.random, name:'Nicolás Meza', image:'%PUBLIC_URL%/People3.png', places:3}
     ];
+    const USERS2=[];
     return <UserList items={USERS}/>
 }
 
