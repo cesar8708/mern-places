3fa4a344e9f25acdd7226fcfd8585825664d093c
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-07-01 00:20:11 -0500

    trabajamos en experimento

--- my-places/src/App.js
+++ my-places/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App=() => {
   return( 
   <Router>
+    <MainNavigation/>
     <Switch>
       <Route path="/" exact={true}>
         <Users/>
@@ -15,6 +17,7 @@ const App=() => {
       </Route>
       <Redirect to="/" />
     </Switch>
+
   </Router>
   );
 } 
