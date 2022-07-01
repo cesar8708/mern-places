7841efd4573030a594313f627e58ec7c41c4aad6
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-06-30 22:13:54 -0500

    Class number 43 ended - we create users components and add styles

--- my-places/src/user/pages/Users.js
+++ my-places/src/user/pages/Users.js
@@ -1,10 +1,16 @@
 import React from "react";
 import UserList from "../components/UserList";
+const images=[
+    "https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-mount-fuji-japan.jpg",
+    "https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-dead-sea-israel-city.jpg",
+    "https://www.planetware.com/wpimages/2019/10/asia-best-places-to-visit-singapore.jpg"
+];
+
 const Users = () => {
     const USERS=[
-        {id:Math.random, name:'Cesar Meza', image:'%PUBLIC_URL%/People1.png', places:1},
-        {id:Math.random, name:'Leidy Quiroz', image:'%PUBLIC_URL%/People2.png', places:2},
-        {id:Math.random, name:'Nicolás Meza', image:'%PUBLIC_URL%/People3.png', places:3}
+        {id:Math.random, name:'Cesar Meza', image:images[0], places:1},
+        {id:Math.random, name:'Leidy Quiroz', image:images[1], places:2},
+        {id:Math.random, name:'Nicolás Meza', image:images[2], places:3}
     ];
     const USERS2=[];
     return <UserList items={USERS}/>
