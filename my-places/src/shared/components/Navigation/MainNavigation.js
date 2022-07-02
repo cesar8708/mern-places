7565a3c506aec6a76bb81288c288803e011de99b
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-07-01 22:48:39 -0500

    class 47 - Mobile Navigation Part 1

--- my-places/src/shared/components/Navigation/MainNavigation.js
+++ my-places/src/shared/components/Navigation/MainNavigation.js
@@ -1,27 +1,35 @@
 import React from "react";
 
 import MainHeader from "./MainHeader";
-import NavLinks  from "./NavLinks";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import { Link } from "react-router-dom";
 
-import './MainNavigation.css';
+import "./MainNavigation.css";
 
-const MainNavigation= props => {
-    return (
-        <MainHeader >
-            <button className="main-navigation__menu-btn">
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/">My Places</Link>
-            </h1>
-            <nav className="main-navigation__header-nav">
-                <NavLinks />
-            </nav>
-        </MainHeader>
-    );
+const MainNavigation = (props) => {
+  return (
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav" >
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">My Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
 };
 
 export default MainNavigation;
