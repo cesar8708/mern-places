e00407bb3f4cdfb5b4fac1fc68c20f14a421d3a6
Cesar Meza Arcia <Cesar8708@gmail.com>
2022-07-05 05:55:43 -0500

    sidebar with animation and functionality

--- my-places/src/shared/components/Navigation/MainNavigation.js
+++ my-places/src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
-import { Link } from "react-router-dom";
+import Backdrop from "../UIElements/Backdrop";
+
 
 import "./MainNavigation.css";
 
+
 const MainNavigation = (props) => {
+  const [drawerIsOpen, SetDrawerIsOpen]= useState(false);
+ 
+  const openDrawerHandler = ()=> {
+    SetDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = ()=>{
+    SetDrawerIsOpen(false);
+  };
   return (
+    
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler}/>:null}
+       
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav" >
           <NavLinks />
         </nav>
       </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
